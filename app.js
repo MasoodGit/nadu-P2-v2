bb864df462179be8eb5310c03db22e678f99242e
MasoodGit <mohamed.alam78@gmail.com>
2014-11-09 22:21:51 +0530

    Add assert function

--- app.js
+++ app.js
@@ -26,18 +26,28 @@ function runTests()
   log(getRelationship());
 }
 
+function runTests_2(){
+  document.getElementById("results").innerHTML = "";
+  assert((getRelationship('x','y') === "Can\'t compare relationships because NaN and NaN are not numbers"),"getRelationship('x','y') : this and that are NaN");
+  assert((getRelationship('x',5) === "Can\'t compare relationships because NaN is not a number"),"getRelationship('x',5) : this is NaN input");
+  assert((getRelationship(15,'asdf') === "Can\'t compare relationships because NaN is not a number"),"getRelationship(15,'asdf')  : that is NaN input");
+  assert((getRelationship(35) === "Can\'t compare relationships because NaN is not a number"),"getRelationship(35) : that is Nan input");
+  assert((getRelationship(3,5) === "<"),"getRelationship(3,5) : this < that");
+  assert((getRelationship(12,5) === ">"),"getRelationship(12,5) : this > that");
+  assert((getRelationship(15,15) ==="=="),"getRelationship(15,15) : this === that");
+  assert((getRelationship(20,5) === ">"), "getRelationship(20,5) : this > that");
+  assert((getRelationship() === "Can\'t compare relationships because NaN and NaN are not numbers"),"getRelationship() : this and that are Nan");
+}
+
+
 function log(message){
   var message = message;
   console.log(message);
   //var consoleDiv = document.getElementById("console");
   
-  var paragraphNode = document.createElement("p");
+  var paragraphNode = document.createElement("li");
   var paragraphContent = document.createTextNode(message);
   paragraphNode.appendChild(paragraphContent);
-
-  //var oldpara = consoleDiv.querySelector('p');
-
-  //consoleDiv.insertBefore(paragraphNode,oldpara);
   document.body.appendChild(paragraphNode);
 }
 
@@ -72,7 +82,7 @@ function getRelationship(x,y){
     result = '>';
   }
   else if(x === y ){
-    result = '=';
+    result = '==';
   }
   
   return result;
